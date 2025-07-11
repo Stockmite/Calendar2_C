fed4cbffeab39a1d1f39e756ca981e5fbf65b33a
Stockmite <rojogreenazul2@gmail.com>
2025-07-11 17:56:43 -0300

    I've updated the Skipline function to make it, on average, more efficient with varying file sizes (I think, I hope, I pray)

--- fileoperations.h
+++ fileoperations.h
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
-#define Predetermined_buffer 15
+#define Predetermined_buffer 50
 
 void SkipLine(FILE* file) { //Yes, these functions are meant for 'r' and other similar file handling modes
 
-    char b = 'b';
+    char * buffer;
+    size_t buffer_lenght = Predetermined_buffer;
 
-    while ((b != EOF) || (b != '\n')) {
+    while (buffer_lenght == (size_t)Predetermined_buffer) {
 
-        b = fgetc(file);
+        fgets(buffer, Predetermined_buffer, file);
+        buffer_lenght = strlen(buffer);
 
     }
 
@@ -18,6 +21,12 @@ void SkipLine(FILE* file) { //Yes, these functions are meant for 'r' and other s
 
 void GetWord(FILE* file) {
 
-    
+    char p_h = 'a';
+
+    while((p_h != EOF) || (p_h != '\n') || (p_h != ' ')) {
+
+        p_h = fgetc(file);
+
+    }
 
 }
